a8a1ea612875397c60cd165793e34dc2dc748bb5
sanzaru <sanzaru@users.noreply.github.com>
2024-03-01 19:14:23 +0100

    Removed old files

--- assets/js/main.js
+++
@@ -1,8 +0,0 @@
-(function () {
-    try {
-        const dateYearNode = document.querySelector('.date-year')
-        if (dateYearNode) { dateYearNode.textContent = String((new Date()).getFullYear()) }
-
-        $('.icon-links a').tooltip({ placement: 'bottom', boundary: 'viewport' })
-    } catch { }
-})()
